81f37c0f0dba325ef57a61d4ff8a449a2d4469f6
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-02-20 13:02:30 -0300

    [feature] Creates get product by category endpoint and fixes database password in development

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async ([FromRoute]string category, ISender sender) =>
+            { 
+                var products = await sender.Send(new GetProductByCategoryQuery(category));
+
+                return Results.Ok(products.Adapt<GetProductByCategoryResponse>());
+            })
+            .WithName("GetProductByCategory")
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get products by category")
+            .WithDescription("Get products by category");
+    }
+}
